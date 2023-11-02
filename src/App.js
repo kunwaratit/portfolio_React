a766c07db21636b24f70e118398dcd81183af643
atit kunwar <atit.191508@ncit.edu.np>
2023-11-02 20:51:47 +0545

    changing

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "./portfolio/Main/Main";
+class App extends React.Component{
+
+    render(){
+     
+      return (<>
+       <Main/>
+      </>) 
+    }}
+export default App
