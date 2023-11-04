ba132d3c3ef177a33b30efb5a014d00f257bbb89
atit kunwar <atit.191508@ncit.edu.np>
2023-11-04 18:40:21 +0545

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Main from "./portfolio/Main/Main";
 class App extends React.Component{
-
-    render(){
-     
+    render(){   
       return (<>
        <Main/>
       </>) 
