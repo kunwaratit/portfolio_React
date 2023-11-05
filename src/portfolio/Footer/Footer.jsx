4e99bf5cbcb9f0b591b30c0322108f30b7e0295b
atit kunwar <atit.191508@ncit.edu.np>
2023-11-05 21:04:10 +0545

    wq

--- src/portfolio/Footer/Footer.jsx
+++ src/portfolio/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./static/footer.css";
 
-function Footer(MenuData){
-    return(<> 
-     <hr/>
+function Footer(MenuData) {
+    return (<>
+        <hr />
         <footer>
             <div className="footer">
 
@@ -12,10 +12,11 @@ function Footer(MenuData){
                         <li><a href="#contact">Contact Us</a> |</li>
                         <li><a href="">Privacy Notice</a> |</li>
                         <li><a href="">FAQs</a> |</li>
+                        <li><a href="https://kunwaratit.github.io/Portfolio/">version 1.0</a>|</li>
                     </ul>
                 </div>
                 <div className="footerRight">copyright&copy;<a href="Portfolio.html">atitKunwar</a></div>
 
             </div>
         </footer></>)
-}export default Footer
+} export default Footer
